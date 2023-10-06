08870cc0a9c7c68cfe72fd258a8ed59056920af4
samuel-anderson <s07241990@gmail.com>
2023-10-05 21:39:48 -0700

    working on stepper

--- src/components/main/content/stepper/stepper.component.jsx
+++ src/components/main/content/stepper/stepper.component.jsx
@@ -1,5 +1,7 @@
 import { Stepper, Step, StepLabel } from "@mui/material";
 import { useSelector } from "react-redux";
+//import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+//import "./stepper.styles.css";
 
 const steps = ["Professional", "Service", "Time", "Done"];
 
@@ -7,7 +9,7 @@ const Steps = () => {
   const activeStep = useSelector((state) => state.step.activeStep);
 
   return (
-    <Stepper activeStep={activeStep} alternativeLabel>
+    <Stepper activeStep={activeStep}>
       {steps.map((label) => (
         <Step key={label}>
           <StepLabel>{label}</StepLabel>
