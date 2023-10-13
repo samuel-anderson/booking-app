2b521835c3a8d514571842ad1c1c42506b9e7efa
samuel-anderson <s07241990@gmail.com>
2023-10-13 13:12:17 -0700

    remove padding

--- src/components/container/container.styles.jsx
+++ src/components/container/container.styles.jsx
@@ -3,4 +3,10 @@ import { styled } from "styled-components";
 export const ContainerStyle = styled.div`
   padding-left: 80px;
   padding-right: 80px;
+
+  @media (max-width: 768px) {
+    & {
+      padding: 0;
+    }
+  }
 `;
