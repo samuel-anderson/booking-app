c90f83a41fc7b734a00de0b8c50523263e17da82
samuel-anderson <s07241990@gmail.com>
2023-10-14 02:12:30 -0700

    cart and scrolling

--- src/components/main/mobile-cart/mobile-car.styles.jsx
+++ src/components/main/mobile-cart/mobile-car.styles.jsx
@@ -7,22 +7,27 @@ export const BottomSheet = styled.div`
     left: 0;
     right: 0;
     background-color: white;
-
-    transition: bottom 0.3s ease-in-out, height 0.3s ease-in-out;
-
     z-index: 999;
     box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.2);
-    padding: 20px;
-
     border-top-left-radius: 5%;
     border-top-right-radius: 5%;
 
-    &.open {
+    transition: bottom 0.5s ease-in-out, height 0.5s ease-in-out;
+
+    &.visible {
       bottom: 0;
       height: 100px;
 
-      &.grow {
-        height: 200px;
+      &.open {
+        height: 100px;
+      }
+
+      &.closed {
+        height: auto;
+      }
+
+      &.expanded {
+        height: 90%;
       }
     }
   }
--- src/components/main/mobile-cart/mobile-cart.component.jsx
+++ src/components/main/mobile-cart/mobile-cart.component.jsx
@@ -4,26 +4,61 @@ import { useSelector } from "react-redux";
 
 const MobileCart = () => {
   const selectedService = useSelector((state) => state.cart.service);
-  const [isExpand, setIsExpandOpen] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const [isScrollinglUp, setIsScrollingUp] = useState(false);
+  const [prevScrollY, setPrevScrollY] = useState(0);
 
   useEffect(() => {
-    if (!selectedService) setIsExpandOpen(false);
+    if (!selectedService) {
+      setIsExpanded(false);
+      setIsScrollingUp(false);
+    }
   }, [selectedService]);
 
   const toggleBottomSheet = () => {
-    setIsExpandOpen(!isExpand);
+    setIsExpanded(!isExpanded);
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (selectedService && !isExpanded) {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > prevScrollY) {
+          setIsScrollingUp(false);
+        } else if (currentScrollY < prevScrollY) {
+          setIsScrollingUp(true);
+        }
+
+        setPrevScrollY(currentScrollY);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      // Clean up the event listener when the component unmounts
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [selectedService, isExpanded, prevScrollY]);
+
+  const cartVisibility = () => {
+    if (isExpanded) return "expanded";
+    else if (isScrollinglUp) return "closed";
+    else return "open";
+  };
+
   return (
     <>
       <BottomSheet>
         <div
-          className={`bottom-sheet ${selectedService ? "open" : ""} ${
-            isExpand ? "grow" : ""
-          }`}
+          className={`bottom-sheet ${
+            selectedService ? "visible" : ""
+          } ${cartVisibility()}`}
         >
-          <button onClick={toggleBottomSheet}>Toggle Bottom Sheet</button>
-          {/* Content for the bottom sheet */}
-          <div className="content">{/* Add your content here */}</div>
+          <button onClick={toggleBottomSheet}>Icon</button>
+          {!isScrollinglUp && <div className="content"></div>}
         </div>
       </BottomSheet>
     </>
