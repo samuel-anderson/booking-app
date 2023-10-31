4cda2780dac03a2cb8938e3d37fcf1930ae41654
samuel-anderson <s07241990@gmail.com>
2023-10-30 22:14:16 -0700

    testing

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA-dixi8-rv-gKhhLZZNprN4R0_ZFU7XAQ",
+  authDomain: "crwn-clothing-db-b150d.firebaseapp.com",
+  projectId: "crwn-clothing-db-b150d",
+  storageBucket: "crwn-clothing-db-b150d.appspot.com",
+  messagingSenderId: "378000436377",
+  appId: "1:378000436377:web:aa4726b00c8870ce2c1c08",
 };
