711b38dc8a31769276172629cc6506e3b9917ed7
samuel-anderson <s07241990@gmail.com>
2023-11-17 00:52:58 -0800

    add role

--- src/data/professionals.js
+++ src/data/professionals.js
@@ -4,6 +4,7 @@ export const professionals = [
     name: "George Mena",
     phoneNumber: "+19097306555",
     email: "george@me.com",
+    role: "user",
     schedule: {
       sunday: [{ start: "8:00 AM", end: "8:00 PM" }],
       monday: [{ start: "8:00 AM", end: "8:00 PM" }],
@@ -19,6 +20,7 @@ export const professionals = [
     name: "Kari Anderson",
     phoneNumber: "+18583543893",
     email: "kari@me.com",
+    role: "user",
     schedule: {
       tuesday: [
         { start: "7:00 AM", end: "9:00 AM" },
@@ -39,6 +41,7 @@ export const professionals = [
     name: "Samuel Anderson",
     phoneNumber: "+17602774923",
     email: "sam@me.com",
+    role: "admin",
     socialMedia: [
       {
         id: "instagram",
