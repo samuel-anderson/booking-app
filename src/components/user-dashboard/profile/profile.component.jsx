a69abfbab541e6202069662c69153abd77d74270
samuel-anderson <s07241990@gmail.com>
2023-11-22 22:11:24 -0800

    profile tab

--- src/components/user-dashboard/profile/profile.component.jsx
+++ src/components/user-dashboard/profile/profile.component.jsx
@@ -1,4 +1,19 @@
+import { selectBarberWithCurrentUser } from "../../../features/professionals/professionalsSelector";
+import { useSelector } from "react-redux";
 const Profile = () => {
-  return <>Profile</>;
+  const currentBarber = useSelector(selectBarberWithCurrentUser);
+  //Registered Kari, need to go to log in
+  //Email isn't saved under current user
+
+  return (
+    <>
+      {currentBarber && (
+        <>
+          Profile: {currentBarber.name}, {currentBarber.email},{" "}
+          {currentBarber.role}
+        </>
+      )}
+    </>
+  );
 };
 export default Profile;
--- src/features/professionals/professionalsSelector.js
+++ src/features/professionals/professionalsSelector.js
@@ -1,7 +1,19 @@
 import { createSelector } from "@reduxjs/toolkit";
 
 const selectBarbers = (state) => state.professionals.barbers;
+const selectCurrentUser = (state) => state.user.currentUser;
 
 export const selectBarberEmails = createSelector(selectBarbers, (barbers) => {
   return barbers.map((barber) => barber.email);
 });
+
+export const selectBarberWithCurrentUser = createSelector(
+  selectBarbers,
+  selectCurrentUser,
+  (barbers, currentUser) => {
+    if (currentUser)
+      return barbers.filter((barber) => barber.email === currentUser.email)[0];
+
+    return null;
+  }
+);
